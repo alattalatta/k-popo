02874696df10efabe61a37a822a6db12f8f98038
alattalatta <urty5656@gmail.com>
2021-12-03 19:17:32 +0900

    Fix eslint-plugin-import incorrectness

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,16 @@
 require('@alattalatta/eslint-config/patch')
 
 module.exports = {
+  root: true,
   extends: ['@alattalatta/eslint-config'],
   parserOptions: {
     project: ['./tsconfig.json'],
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {
+        project: './tsconfig.json',
+      },
+    },
   },
 }
