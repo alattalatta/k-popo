f5ee95f74193ead97085c519a11778e279f2ba9f
alattalatta <urty5656@gmail.com>
2021-12-03 21:26:21 +0900

    Add josa to the benchmark

--- benchmark/benchmark.ts
+++ benchmark/benchmark.ts
@@ -2,38 +2,33 @@ import text from '@ko_kr/text'
 import { josa as autoJosa } from 'auto-josa'
 import b from 'benny'
 import faker from 'faker'
+import { josa } from 'josa'
 import { ko } from 'k-popo'
 
 faker.locale = 'ko'
 
-const run = (fn: (name: string) => void): (() => void) => {
-  return () => {
-    fn(faker.name.firstName())
-  }
-}
+const name = (): string => faker.name.firstName()
 
 void b.suite(
   'Benchmark',
 
-  b.add('k-popo', () =>
-    run(
-      (name) =>
-        void ko`Lorem ipsum dolor ${name}(이)가 amet, consectetur adipiscing elit. ${name}(이)가 Nulla pulvinar iaculis risus`,
+  b.add(
+    'k-popo',
+    () => ko`${name()}(이)가 ${name()}(을)를 ${name()}(이)가 ${name()}(을)를 ${name()}(이)가 ${name()}(을)를`,
+  ),
+  b.add(
+    '@ko_kr/text',
+    () => text`${name()}이 ${name()}을 ${name()}이 ${name()}을 ${name()}이 ${name()}을 ${name()}이 ${name()}을`,
+  ),
+  b.add(
+    'auto-josa',
+    () => autoJosa`${name()}이 ${name()}을 ${name()}이 ${name()}을 ${name()}이 ${name()}을 ${name()}이 ${name()}을`,
+  ),
+  b.add('josa', () =>
+    josa(
+      `${name()}#{이} ${name()}#{을} ${name()}#{이} ${name()}#{을} ${name()}#{이} ${name()}#{을} ${name()}#{이} ${name()}#{을}`,
     ),
   ),
-  b.add('@ko_kr/text', () =>
-    run(
-      (name) =>
-        void text`Lorem ipsum dolor ${name}이 amet, consectetur adipiscing elit. ${name}이 Nulla pulvinar iaculis risus`,
-    ),
-  ),
-  b.add('auto-josa', () =>
-    run(
-      (name) =>
-        void autoJosa`Lorem ipsum dolor ${name}이 amet, consectetur adipiscing elit. ${name}이 Nulla pulvinar iaculis risus`,
-    ),
-  ),
-
   b.cycle(),
   b.complete(),
   b.save({ file: 'result', format: 'table.html' }),
